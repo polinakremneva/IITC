e540b7fba33e2dcf69aa4a229ed4ec617e20ef07
polinakremneva <polinaapolly@gmail.com>
2024-04-21 15:23:09 +0300

    second

--- html-css-js-dom-mini-projects/bulls-and-cows/index.js
+++ html-css-js-dom-mini-projects/bulls-and-cows/index.js
@@ -3,7 +3,18 @@ const computerBoard = [];
 let playerBoard = [];
 let roundCounter = 0;
 win = false;
+const results = [];
+let currentPlayer = {
+  name: "", 
+  attempts: 0, 
+};
+
 // flow
+function askPlayerName() {
+  currentPlayer.name = prompt("Put your name:"); 
+  currentPlayer.attempts = 0; 
+}
+
 function generateComputerBoard() {
   const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   for (let i = 0; i < 4; i++) {
@@ -57,7 +68,6 @@ function appendToDisplay(value) {
         playerBoard[3] = value;
         if (playerBoard[roundCounter] === 0) {
           playerBoard[roundCounter] = "0";
-          r;
         }
         document.getElementById("display4").value = value;
       }
@@ -92,9 +102,16 @@ function cowCheck() {
 function checkBoard() {
   bullCheck();
   cowCheck();
-  if (playerBoard.toString() === computerBoard.toString()) {
+  currentPlayer.attempts++; 
+
+  if (playerBoard.toString() == computerBoard.toString()) {
     win = true;
-    alert("You win!");
+    alert("You win with " + currentPlayer.attempts + " attempts!");
+    
+    results.push({ name: currentPlayer.name, attempts: currentPlayer.attempts }); 
+    console.table(results); 
+
+    resetGame(); 
   }
 
   if (roundCounter === 4) {
@@ -109,10 +126,10 @@ function addElement() {
   newRow.id = "row";
   newRow.innerHTML = `
     <div id="left">
-      <input class="window" value="${playerBoard[0] || ""}" />
-      <input class="window" value="${playerBoard[1] || ""}" />
-      <input class="window" value="${playerBoard[2] || ""}" />
-      <input class="window" value="${playerBoard[3] || ""}" />
+      <input class="window" value="${playerBoard[0] }" />
+      <input class="window" value="${playerBoard[1] }" />
+      <input class="window" value="${playerBoard[2] }" />
+      <input class="window" value="${playerBoard[3] }" />
     </div>
     <div class="right">
       <button onclick="checkBoard()">Go</button>
@@ -121,4 +138,39 @@ function addElement() {
   document.getElementById("rows").appendChild(newRow);
 }
 
+function resetGame() {
+  computerBoard.length = 0; 
+  playerBoard = []; 
+  roundCounter = 0;
+  win = false; 
+
+  generateComputerBoard(); 
+
+  document.getElementById("display1").value = "";
+  document.getElementById("display2").value = "";
+  document.getElementById("display3").value = "";
+  document.getElementById("display4").value = "";
+
+  document.querySelector(".bull").innerText = "Bulls:";
+  document.querySelector(".cow").innerText = "Cows:";
+
+  
+  const rowsElement = document.getElementById("rows");
+  while (rowsElement.children.length > 2) {
+    rowsElement.removeChild(rowsElement.lastChild);
+  }
+
+  askPlayerName();
+}
+
+function endGame() {
+  if (currentPlayer.name) {
+    results.push({ name: currentPlayer.name, attempts: currentPlayer.attempts }); 
+  }
+  alert("You ended the game!");
+  console.table(results); 
+  window.close(); 
+}
+
 generateComputerBoard();
+askPlayerName()
